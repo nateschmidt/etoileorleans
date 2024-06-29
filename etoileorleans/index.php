7bcbe99c60be7a58a43d170d76fab42c68cfcc8d
Nate Schmidt <nate@nateschmidt.com>
2024-06-29 13:59:37 -0500

    initial commit

---
+++ etoileorleans/index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Étoile</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="./styles.css" />
+  </head>
+
+  <body
+    class="py-3 d-flex flex-column justify-content-between"
+    style="min-height: 100vh"
+  >
+    <div class="container-sm">
+      <div class="d-flex justify-content-center align-items-center">
+        <a
+          href="/"
+          class="text-center text-decoration-none text-dark h1 py-2 py-sm-4 text-primary-custom"
+        >
+          Étoile
+        </a>
+      </div>
+      <div class="d-flex flex-column-reverse flex-sm-column gap-4">
+        <div class="">
+          <img class="img-fluid" src="./assets/home.png" alt="" />
+        </div>
+
+        <div
+          class="d-flex flex-wrap flex-column align-items-center flex-sm-row gap-2 justify-content-center text-primary-custom"
+        >
+          <a
+            href="./resturant.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >RESTAURANT</a
+          >
+
+          <div class="d-none d-sm-block">|</div>
+
+          <a
+            href="./chef.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >Chef</a
+          >
+          <div class="d-none d-sm-block">|</div>
+
+          <a
+            href="./building.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >Building</a
+          >
+          <div class="d-none d-sm-block">|</div>
+
+          <a
+            href="./index.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >Reservations</a
+          >
+          <div class="d-none d-sm-block">|</div>
+
+          <a
+            href="./menu.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >MENU</a
+          >
+          <div class="d-none d-sm-block">|</div>
+
+          <a
+            href="./index.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >Gift Card</a
+          >
+          <div class="d-none d-sm-block">|</div>
+
+          <a
+            href="./private-events.html"
+            class="link-dark text-primary-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >Private Events</a
+          >
+        </div>
+      </div>
+    </div>
+
+    <footer class="container-sm py-sm-5">
+      <div
+        class="container-md d-sm-flex justify-content-center align-items-center d-none"
+      >
+        <a
+          href="/"
+          class="text-center text-muted-custom text-decoration-none h4 py-1"
+        >
+          Étoile
+        </a>
+      </div>
+
+      <div
+        class="d-flex flex-column flex-wrap align-items-center flex-sm-row py-3 justify-content-center gap-2 text-muted-custom"
+      >
+        <a
+          href="/"
+          class="link-dark text-muted-custom fw-semibold text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+          ><small>3607 Magazine St</small></a
+        >
+
+        <div class="d-none d-sm-block">|</div>
+
+        <a
+          href="/"
+          class="link-dark text-muted-custom fw-semibold text-muted text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+          ><small>New Orleans, LA 70115</small></a
+        >
+        <div class="d-none d-sm-block">|</div>
+
+        <a
+          href="tel:504-582-9920"
+          class="link-dark text-muted-custom fw-semibold text-muted text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+          ><small>(504) 582-9920</small></a
+        >
+        <div class="d-none d-sm-block">|</div>
+
+        <a
+          href="mailto:etoileorleans@gmail.com"
+          class="link-dark text-muted-custom fw-semibold text-muted text-uppercase hover-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+          ><small>etoileorleans@gmail.com</small></a
+        >
+      </div>
+      <div class="d-flex flex-row py-1 justify-content-center gap-2">
+        <a
+          href="/facebook"
+          class="py-0 text-decoration-none d-flex justify-content-center align-items-center rounded-circle social-hover"
+          ><i class="fa-brands fa-facebook-f"></i></a
+        ><a
+          href="/facebook"
+          class="py-0 d-flex text-decoration-none justify-content-center align-items-center rounded-circle social-hover"
+          ><i class="fa-brands fa-x-twitter"></i></a
+        ><a
+          href="/facebook"
+          class="py-0 text-decoration-none d-flex justify-content-center align-items-center rounded-circle social-hover"
+          ><i class="fa-brands fa-youtube"></i></a
+        ><a
+          href="/facebook"
+          class="py-0 text-decoration-none d-flex justify-content-center align-items-center rounded-circle social-hover"
+          ><i class="fa-brands fa-instagram"></i
+        ></a>
+      </div>
+    </footer>
+  </body>
+</html>
