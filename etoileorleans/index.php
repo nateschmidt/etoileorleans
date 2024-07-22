12a6695445fc0791e3c4d619bb807de2c343d7d7
Nate Schmidt <nateschmidt@Nates-Mac-Studio-2.local>
2024-07-22 12:51:19 -0500

    update font and events

--- etoileorleans/index.php
+++ etoileorleans/index.php
@@ -22,7 +22,9 @@
       crossorigin="anonymous"
       referrerpolicy="no-referrer"
     />
-    <link rel="stylesheet" href="./styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Noticia+Text:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   </head>
 
   <body
