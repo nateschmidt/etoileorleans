473e2a146296b284a1ccbbdcf3acb837ca1cd2b0
Nate Schmidt <nateschmidt@Nates-Mac-Studio-2.local>
2024-10-14 17:02:27 -0500

    update font to cardo

--- etoileorleans/index.php
+++ etoileorleans/index.php
@@ -24,7 +24,8 @@
     />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Noticia+Text:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&display=swap" rel="stylesheet">
+
 
     <link rel="stylesheet" href="./styles.css" />
   </head>
